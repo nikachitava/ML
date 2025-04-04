86d1cbbe0cfed959938ebf3c4c1887cbba184534
nikachitava18 <91054632+nikachitava@users.noreply.github.com>
2025-04-04 17:59:25 +0400

    Update lesson_4.py

--- lesson_4.py
+++ lesson_4.py
@@ -1,13 +1,13 @@
-# f = open("files/text.txt", "w")
-# f.write("test string")
-# f.close()
-
 lines_count = 0
+words_count = 0
+
 f = open("files/text.txt", "r")
 lines = f.readlines()
 for i in lines:
     lines_count += 1
+    words_count += len(i.split())
 f.close()
 
 
 print("ტექსტში არის " + str(lines_count) + " სტრიქონი")
+print("ტექსტში არის " + str(words_count) + " სიტყვა")
