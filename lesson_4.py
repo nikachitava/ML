32d7daf83e959787cf3c440be06c12d5cf27ac73
nikachitava18 <91054632+nikachitava@users.noreply.github.com>
2025-04-04 18:36:12 +0400

    Update lesson_4.py

--- lesson_4.py
+++ lesson_4.py
@@ -1,3 +1,17 @@
+def most_common_word(t):
+    counts = {}
+    for word in t.split():
+        word = word.lower()
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    most_common = max(counts, key=counts.get)
+    return most_common
+
+
+
 lines_count = 0
 words_count = 0
 unique_word_count = 0
@@ -20,10 +34,11 @@ for i in lines:
 text = text.lower()
 
 unique_word_count = len(unique_words)
-
+most_common = most_common_word(' '.join(lines))
 f.close()
 
 
 print("ტექსტში არის " + str(lines_count) + " სტრიქონი")
 print("ტექსტში არის " + str(words_count) + " სიტყვა")
 print("ტექსტში არის " + str(unique_word_count) + " უნიკალური სიტყვა")
+print("ტექსტში ყველაზე ხშირად გამოყენებული სიტყვა: " + most_common)
