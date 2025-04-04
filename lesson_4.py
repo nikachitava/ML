10a496f467a17f7b359d09e4b1fe8857f604a82e
nikachitava18 <91054632+nikachitava@users.noreply.github.com>
2025-04-04 18:25:51 +0400

    Update lesson_4.py

--- lesson_4.py
+++ lesson_4.py
@@ -1,13 +1,29 @@
 lines_count = 0
 words_count = 0
+unique_word_count = 0
+
+unique_words = set()
+
 
 f = open("files/text.txt", "r")
 lines = f.readlines()
+text = f.read()
+
 for i in lines:
     lines_count += 1
     words_count += len(i.split())
+
+    words_in_line = i.split()
+    unique_words.update(word.lower() for word in words_in_line)
+
+
+text = text.lower()
+
+unique_word_count = len(unique_words)
+
 f.close()
 
 
 print("ტექსტში არის " + str(lines_count) + " სტრიქონი")
 print("ტექსტში არის " + str(words_count) + " სიტყვა")
+print("ტექსტში არის " + str(unique_word_count) + " უნიკალური სიტყვა")
